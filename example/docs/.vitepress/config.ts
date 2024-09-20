928dc2928f70e34c974282b5ca329354ec928f45
BeiyanYunyi <14120445+BeiyanYunyi@users.noreply.github.com>
2024-09-20 23:18:27 +0800

    feat: :sparkles: add example

---
+++ example/docs/.vitepress/config.ts
@@ -0,0 +1,25 @@
+import genConfig from '@project-trans/vitepress-theme-project-trans/config'
+import type { SidebarOptions } from '@project-trans/vitepress-theme-project-trans/theme'
+import type { ThemeContext } from '@project-trans/vitepress-theme-project-trans/utils'
+import { withThemeContext } from '@project-trans/vitepress-theme-project-trans/utils'
+import type { DefaultTheme } from 'vitepress'
+
+const nav: DefaultTheme.NavItem[] = []
+
+const sidebarOptions: SidebarOptions = []
+
+const themeConfig: ThemeContext = {
+  siteTitle: 'RLE.wiki',
+  siteDescription: '一份 RLE 指北',
+  /** Repo */
+  githubRepoLink: 'https://github.com/project-trans/RLE-wiki',
+  /** vitepress 根目录 */
+  rootDir: 'docs',
+  /** 文档所在目录（目前似未使用此项） */
+  include: ['campus', 'contributor-guide', 'fashion'],
+  nav,
+  sidebarOptions,
+}
+
+// https://vitepress.dev/reference/site-config
+export default withThemeContext(themeConfig, genConfig)
---
+++ example/docs/.vitepress/theme/index.ts
@@ -0,0 +1,10 @@
+// docs/.vitepress/theme/index.ts
+// https://vitepress.dev/guide/custom-theme
+import PtjsTheme from '@project-trans/vitepress-theme-project-trans/theme'
+
+import 'uno.css'
+import './style.css'
+
+export default {
+  extends: PtjsTheme,
+}
