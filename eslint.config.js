58938e88434a322e8a689dd1edac79d5379d3c17
BeiyanYunyi <14120445+BeiyanYunyi@users.noreply.github.com>
2024-09-21 16:20:02 +0800

    fix(AppFooter): :bug: no SuggestionBox

--- eslint.config.js
+++ eslint.config.js
@@ -2,7 +2,7 @@ import antfu from '@antfu/eslint-config'
 
 export default antfu({
   formatters: true,
-  unocss: true,
+  // unocss: true,
   typescript: true,
   vue: true,
   ignores: [
--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -1,27 +1,28 @@
 <script setup lang="ts">
-import { ref, watch, } from 'vue';
-import { NolebaseGitChangelog } from '@nolebase/vitepress-plugin-git-changelog/client';
-import { useRoute } from 'vitepress';
+import { NolebaseGitChangelog } from '@nolebase/vitepress-plugin-git-changelog/client'
+import { useRoute } from 'vitepress'
+import { ref, watch } from 'vue'
+import { AppSBox } from '../components'
 
-const route = useRoute();
+const route = useRoute()
 
 // 定义一个 ref 来存储动态 key
-const componentKey = ref(0);
-const frontmatter = ref({});
-const isFrontmatterLoaded = ref(false);
+const componentKey = ref(0)
+const frontmatter = ref({})
+const isFrontmatterLoaded = ref(false)
 
 // 更新 key 和 frontmatter 的函数
-const updateKeyAndFrontmatter = () => {
-  componentKey.value += 1;
-  frontmatter.value = route.data?.frontmatter || {};
-  isFrontmatterLoaded.value = true;
-};
+function updateKeyAndFrontmatter() {
+  componentKey.value += 1
+  frontmatter.value = route.data?.frontmatter || {}
+  isFrontmatterLoaded.value = true
+}
 
 // 监听路由变化，更新 key 和 frontmatter
 watch(() => route.path, () => {
-  isFrontmatterLoaded.value = false;
-  updateKeyAndFrontmatter();
-}, { immediate: true }); // 在组件挂载时立即执行一次，确保第一次渲染时 key 和 frontmatter 是正确的
+  isFrontmatterLoaded.value = false
+  updateKeyAndFrontmatter()
+}, { immediate: true }) // 在组件挂载时立即执行一次，确保第一次渲染时 key 和 frontmatter 是正确的
 
 // 在组件挂载时更新 key 和 frontmatter
 // onMounted(updateKeyAndFrontmatter);
