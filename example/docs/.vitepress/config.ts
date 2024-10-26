7d718e409a5072405ebefda72fe834f89ee2d954
Lee <lee@mtftm.com>
2024-10-26 22:47:01 +0800

    feat: 支持自定义文档属性栏 (#12)
    ### 可在 config 文件内配置以下选项:
    
    - HideReadingTime: true, /* 隐藏字数和预计阅读时间 */
    - HideLastUpdated: true, /* 隐藏最后更新时间 */
    - HideAuthors: true, /* 隐藏作者信息 */

--- example/docs/.vitepress/config.ts
+++ example/docs/.vitepress/config.ts
@@ -1,32 +1,32 @@
-import genConfig from '@project-trans/vitepress-theme-project-trans/config'
 import type { SidebarOptions } from '@project-trans/vitepress-theme-project-trans/theme'
 import type { ThemeContext } from '@project-trans/vitepress-theme-project-trans/utils'
-import { withThemeContext } from '@project-trans/vitepress-theme-project-trans/utils'
 import type { DefaultTheme } from 'vitepress'
+import genConfig from '@project-trans/vitepress-theme-project-trans/config'
+import { withThemeContext } from '@project-trans/vitepress-theme-project-trans/utils'
 
 const nav: NavConfig = [
   {
-    text: "大学指南",
-    link: "/campus/",
+    text: '大学指南',
+    link: '/campus/',
   },
   {
-    text: "贡献指南",
+    text: '贡献指南',
     items: [
       {
-        text: "校园版块投稿指南",
-        link: "/contributor-guide/campus.md",
+        text: '校园版块投稿指南',
+        link: '/contributor-guide/campus.md',
       },
       {
-        text: "其他投稿指南",
-        link: "/contributor-guide/other.md",
+        text: '其他投稿指南',
+        link: '/contributor-guide/other.md',
       },
       {
-        text: "校园版块贡献模板",
-        link: "/contributor-guide/CampusTemplate.md",
+        text: '校园版块贡献模板',
+        link: '/contributor-guide/CampusTemplate.md',
       },
     ],
   },
-];
+]
 
 const baseConfig = {
   useTitleFromFrontmatter: true,
@@ -42,7 +42,7 @@ const sidebarOptions = [
     scanStartPath: 'campus',
     resolvePath: '/campus/',
     sortMenusByFrontmatterOrder: true,
-  }
+  },
 ]
 
 const themeConfig: ThemeContext = {
@@ -59,6 +59,9 @@ const themeConfig: ThemeContext = {
   sidebarOptions,
   // enableChangeLog: false,
   enableSuggestionBox: false,
+  // HideReadingTime: true, /* 隐藏字数和预计阅读时间 */
+  // HideLastUpdated: true, /* 隐藏最后更新时间 */
+  // HideAuthors: true, /* 隐藏作者信息 */
 }
 
 // https://vitepress.dev/reference/site-config
