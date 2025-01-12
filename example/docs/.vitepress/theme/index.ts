b16b3ace0ab0f414743fdb89ac282ca053dcb9f8
dongguacute <gudupao2022@gmail.com>
2025-01-12 11:12:59 +0800

    更新图片缩放功能

--- example/docs/.vitepress/theme/index.ts
+++ example/docs/.vitepress/theme/index.ts
@@ -4,7 +4,25 @@ import PtjsTheme from '@project-trans/vitepress-theme-project-trans/theme'
 
 import 'uno.css'
 import './style.css'
-
+import mediumZoom from 'medium-zoom';
+import { onMounted, watch, nextTick } from 'vue';
+import { useRoute } from 'vitepress';
 export default {
   extends: PtjsTheme,
+
+  // 为所有图像启用缩放功能
+  setup() {
+    const route = useRoute();
+    const initZoom = () => {
+      // mediumZoom('[data-zoomable]', { background: 'var(--vp-c-bg)' }); // 默认
+      mediumZoom('.main img', { background: 'var(--vp-c-bg)' }); // 不显式添加{data-zoomable}的情况下为所有图像启用此功能
+    };
+    onMounted(() => {
+      initZoom();
+    });
+    watch(
+      () => route.path,
+      () => nextTick(() => initZoom())
+    );
+  },
 }
