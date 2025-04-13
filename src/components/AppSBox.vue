2f8ce93b4607d7628a32dd9568fc79a67a67d519
BeiyanYunyi <14120445+BeiyanYunyi@users.noreply.github.com>
2025-04-13 18:26:29 +0800

    chore: :art: change suggestion box url

--- src/components/AppSBox.vue
+++ src/components/AppSBox.vue
@@ -5,6 +5,6 @@ import SuggestionBox from '@project-trans/suggestion-box/component'
 <template>
   <SuggestionBox
     text-content-placeholder="这个页面有什么问题？或者有什么建议？请在下方留言，我们会尽快回复和处理。"
-    target-url="/api/v1/suggestion"
+    target-url="https://suggestion-box.project-trans.org/api/v1/suggestion"
   />
 </template>
