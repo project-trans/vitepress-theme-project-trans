31fc9256833884f5d979d2313f1238cd3a22114d
BeiyanYunyi <14120445+BeiyanYunyi@users.noreply.github.com>
2025-10-13 15:20:12 +0800

    chore(deps): :arrow_up: upgrade suggestion box to 0.6.1

--- src/components/AppSBox.vue
+++ src/components/AppSBox.vue
@@ -8,3 +8,19 @@ import SuggestionBox from '@project-trans/suggestion-box/component'
     target-url="https://suggestion-box.project-trans.org/api/v1/suggestion"
   />
 </template>
+
+<style scoped>
+/* to set suggestionbox's style */
+form {
+  --c-bg-opacity: 1;
+  --c-border-light: rgb(228 228 231 / var(--c-bg-opacity));
+  --c-border-dark: rgb(39 39 42 / var(--c-bg-opacity));
+  --c-textarea-bg-light: rgb(255 255 255 / var(--c-bg-opacity));
+  --c-textarea-bg-dark: #1b1b1f;
+  --c-textarea-disabled-text: rgb(161 161 170);
+  --c-contact-bg-light: rgb(244 244 245 / var(--c-bg-opacity));
+  --c-contact-bg-dark: rgb(24 24 27 / var(--c-bg-opacity));
+  --c-action-bg-light: rgb(250 250 250 / var(--c-bg-opacity));
+  --c-action-bg-dark: rgb(24 24 27 / var(--c-bg-opacity));
+}
+</style>
